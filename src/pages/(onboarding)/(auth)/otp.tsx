99d738a265d7b5cb226e996feb4e9087e034580d
Samuel Mensah <samuelmensqh@gmail.com>
2025-01-30 23:07:54 +0000

    unbording-003

--- src/pages/(onboarding)/(auth)/otp.tsx
+++ src/pages/(onboarding)/(auth)/otp.tsx
@@ -5,46 +5,55 @@ import {
   InputOTPSlot,
 } from "../../../../components/ui/input-otp";
 import { useNavigate } from "react-router-dom";
+import OTPVerified from "../../../components/ui/OTPVerified";
+import { useState } from "react";
 
 const OTPVerification = () => {
   const navigate = useNavigate();
+  const [isVerified, setIsVerified] = useState(true);
   return (
-    <div className="flex flex-col gap-4 justify-center items-center md:w-3/5 mx-auto">
-      <div>
-        <div className="w-12 h-12 mx-auto rounded-full bg-gray-500 mb-4"></div>
-        <h5>OTP (One Time Password)</h5>
-      </div>
-      <div>
-        <p className="text-gray-400 mb-6 text-center">
-          An OTP has been sent to ******km@gmail.com. Please type the 4-digit
-          code below.
-        </p>
-        <div className="flex flex-col items-center justify-center gap-4">
-          <InputOTP maxLength={4}>
-            <InputOTPGroup className="flex justify-center gap-4 mb-4">
-              <InputOTPSlot index={0}></InputOTPSlot>
+    <>
+      {isVerified ? (
+        <OTPVerified />
+      ) : (
+        <div className="flex flex-col gap-4 justify-center items-center md:w-3/5 mx-auto">
+          <div>
+            <div className="w-12 h-12 mx-auto rounded-full bg-gray-500 mb-4"></div>
+            <h5>OTP (One Time Password)</h5>
+          </div>
+          <div>
+            <p className="text-gray-400 mb-6 text-center">
+              An OTP has been sent to ******km@gmail.com. Please type the
+              4-digit code below.
+            </p>
+            <div className="flex flex-col items-center justify-center gap-4">
+              <InputOTP maxLength={4}>
+                <InputOTPGroup className="flex justify-center gap-4 mb-4">
+                  <InputOTPSlot index={0}></InputOTPSlot>
 
-              <InputOTPSlot index={1}></InputOTPSlot>
+                  <InputOTPSlot index={1}></InputOTPSlot>
 
-              <InputOTPSlot index={2}></InputOTPSlot>
+                  <InputOTPSlot index={2}></InputOTPSlot>
 
-              <InputOTPSlot index={3}></InputOTPSlot>
-            </InputOTPGroup>
-          </InputOTP>
+                  <InputOTPSlot index={3}></InputOTPSlot>
+                </InputOTPGroup>
+              </InputOTP>
 
-          <Button type="submit" className="w-full">
-            Verify OTP
-          </Button>
-          <Button
-            variant="ghost"
-            className="w-full"
-            onClick={() => navigate(-1)}
-          >
-            Go Back
-          </Button>
+              <Button type="submit" className="w-full">
+                Verify OTP
+              </Button>
+              <Button
+                variant="ghost"
+                className="w-full"
+                onClick={() => navigate(-1)}
+              >
+                Go Back
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
